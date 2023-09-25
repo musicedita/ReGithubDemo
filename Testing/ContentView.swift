c5783bc92cfb6eaefbe42885a09e24d9c5dec9c6
Joyce <lajo6780@gmail.com>
2023-09-24 21:48:25 -0500

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by student1 on 9/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by student1 on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
